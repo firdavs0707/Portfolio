029a47f95b67b7a5eec3027199798ba3cdbf39f8
firdavs0707 <firdavs0.4.2.0.0.7>
2022-10-09 18:26:12 +0500

    Add codes

--- send.js
+++ send.js
@@ -1,15 +1,19 @@
+
+
 //bot token
 var telegram_bot_id = "5652103664:AAHTWDIBuqnMN1lT1lPsMMeUL8o0QU1qpHI"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
 //chat id
 var chat_id = 1191155807; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
-var u_name, email, coun, tel, message;
+var u_name, email, coun, tel, tel2, tel3, message;
 var ready = function() {
-    u_name = document.getElementById("name").value;
-    email = document.getElementById("email").value;
-    coun = document.getElementById("coun").value;
-    tel = '+998' + document.getElementById("telcodi").value + document.querySelector("tel-body").value;
-    message = document.getElementById("message").value;
-    message = "Ismi: " + u_name + "\nEmail: " + email + "\nDavlat: " + coun + "\nTelefon raqami: " + tel + "\nXabar: " + message;
+  u_name = document.getElementById("name").value;
+  email = document.getElementById("email").value;
+  coun = document.getElementById("coun").value;
+  tel = document.getElementById("telcodi").value;
+  tel2 = document.getElementById("telli").value;
+  tel3 = '+998' + tel + tel2;
+  message = document.getElementById("message").value;
+  message = "Ismi: " + u_name + "\nEmail: " + email + "\nDavlat: " + coun + "\nTelefon raqami: " + tel3 + "\nXabar: " + message;
 };
 
 var sendtelegram = function() {
